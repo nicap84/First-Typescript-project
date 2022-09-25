012b25c27f65c848c00775b36c480c9da2d228c9
Maria Marin <mtmarin84@gmail.com>
2022-09-25 20:31:23 +0200

    create a new webpack config for production

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
-
-import { ProjectInput } from './components/project-input'
-import { ProjectList } from './components/project-list'
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list';
 
 new ProjectInput();
 new ProjectList('active');
